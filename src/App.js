75abfd1c0b3ccb600d67a3a8b4f9195f39eeede6
Brazenbillygoat <hyrum.butler3@gmail.com>
2021-02-02 09:35:25 -0500

    Redux added as a dependency and imported

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
+
+
+
+
+
+
+
+  
   return (
     <div className="App">
       <header className="App-header">
